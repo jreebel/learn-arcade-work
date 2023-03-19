4414b3c8052b94591cce1d773eed98a82d5c0110
jreeb <jreebel@gmail.com>
2023-03-19 17:38:46 -0400

    Complete as in the lesson

--- ScratchWork/collect_coins.py
+++ ScratchWork/collect_coins.py
@@ -2,7 +2,7 @@ import random
 import arcade
 
 SPRITE_SCALING_PLAYER = 0.5
-SPRITE_SCALING_COIN = 0.2
+SPRITE_SCALING_COIN = 0.3
 COIN_COUNT = 50
 
 SCREEN_WIDTH = 800
@@ -37,12 +37,35 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
+        # create the coins
+
+        for i in range(COIN_COUNT):
+            coin = arcade.Sprite("coin.png", SPRITE_SCALING_COIN)
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+            self.coin_list.append(coin)
+
     def on_draw(self):
         arcade.start_render()
 
         self.coin_list.draw()
         self.player_list.draw()
+        output = "Score: " + str(self.score)
+        arcade.draw_text(output, 10, 20, arcade.color.RED, 14)
 
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time: float):
+        """ Movement and game logic """
+        self.coin_list.update()
+
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+        # loop thru the hit sprites, remove them and update score
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
 
 def main():
     window = MyGame()
