6f439a492ff47d9f2b21d9c67fbaee0ec6f8ace3
jreeb <jreebel@gmail.com>
2023-03-25 12:00:28 -0400

    initial grid green cell

--- ScratchWork/array_backed_grid.py
+++ ScratchWork/array_backed_grid.py
@@ -20,17 +20,29 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        # create 10 x 10 array, fill with zeroes
+        self.grid = []
+        for row in range(ROW_COUNT):
+            self.grid.append([])
+            for column in range(COLUMN_COUNT):
+                self.grid[row].append(0)
+
     def on_draw(self):
         """
         Render the screen.
         """
         arcade.start_render()
+        
         for row in range(ROW_COUNT):
             y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT / 2
-
             for column in range(COLUMN_COUNT):
                 x = (MARGIN + WIDTH) * column + MARGIN + WIDTH / 2
-                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, arcade.color.WHITE)
+
+                color = arcade.color.WHITE
+                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
+                if self.grid[row][column] == 1:
+                    color = arcade.color.GREEN
+                    arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
@@ -41,6 +53,7 @@ class MyGame(arcade.Window):
 
 def main():
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.grid[1][5] = 1
     arcade.run()
 
 
